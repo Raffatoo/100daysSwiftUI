4009442d87a03f640cf8e9714e3c13372a02a792
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2022-04-04 13:05:58 -0500

    Annotations
    Type annotations and checkpoint 2

---
+++ 004day/Anotaciones.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Swift es
+
+let surname = "Lasso"
+var score = 0
+
+// la anotación explicit
+let surnamee : String = "Lasso"
+var scoree: Int = 0
+
+// Usando los enum
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+
+
+let teams = ["Real Madrid", "Barcelona", "Atletico", "Sevilla", "valencia", "Málaga"]
+
+print("Son \(teams.count) Equipos Listados")
+
+
+
+
